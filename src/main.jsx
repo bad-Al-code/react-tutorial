4dce2ad9a9ff954625900b4e5211e0f7b97b62c8
badAl <badalsingh120403@gmail.com>
2024-06-21 18:36:24 +0530

    clean: remove customReact

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
-function MyApp() {
-  return (
-    <>
-      <h1>MyApp</h1>
-    </>
-  );
-}
-
-const anotherUser = "chai aur react";
-const ReactElement = React.createElement(
-  "a",
-  {
-    href: "https://www.google.com",
-    target: "_blank",
-  },
-  "click me to visit google",
-  anotherUser
-);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <App />
-  // <MyApp />
-  ReactElement
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
