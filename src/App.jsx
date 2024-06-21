897582491b18f63f170b65849ee829ec96a2cc2a
badAl <badalsingh120403@gmail.com>
2024-06-22 01:39:34 +0530

    remove: remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import "./index.css";
-import { SignInTwo } from "./components/SignIn";
 
 function App() {
-  let myObj = {
-    username: "Al",
-    age: 19,
-  };
-
-  let myArray = [1, 3, 5];
   return (
     <>
       <h1 className="text-xl bg-cyan-300 p-4 text-center m-4 rounded-lg shadow-lg border border-cyan-800">
         Hello
       </h1>
-      <SignInTwo name="badAl" btnText="Al" />
-      <SignInTwo name="Al" />
     </>
   );
 }
