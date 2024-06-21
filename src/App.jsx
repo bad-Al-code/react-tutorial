c84207adfdc8a31a6d3da78c2b4e4e3c5e9bfa18
badAl <badalsingh120403@gmail.com>
2024-06-21 19:03:43 +0530

    feat: complete counterApp using useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 function App() {
-  const username = "chai aur code";
+  let [counter, setCounter] = useState(5);
+  const username = "chai aur react";
+  // let counter = 5;
+
+  const addValue = () => {
+    counter += 1;
+    setCounter(counter);
+    console.log(counter);
+    console.log("clicked", Math.random());
+  };
+
+  const removeValue = () => {
+    if (counter <= 0) {
+      alert("counter cannot not be less than 0");
+    }
+    setCounter(counter - 1);
+  };
 
   return (
     <>
       <h1>Hello {username}</h1>
+      <h3>Counter Value: {counter}</h3>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
     </>
   );
 }
